5940cedf1e4d6fa871305bd94f3fe2de4033eb0b
dottv4 <143780081+dottv4@users.noreply.github.com>
2023-09-26 10:53:07 +0200

    Finish RPS

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-//RPS GAME
-//MAKE COMPUTER PICK RANDOM CHOICE
-//MAKE PLAY ROUND FUNCTION
-
-const computerSelection = getComputerChoice();
 const rockBtn = document.querySelector('#rock');
 const paperBtn = document.querySelector('#paper');
 const scissorsBtn = document.querySelector('#scissors');
+const resetBtn = document.querySelector('#reset');
+const container = document.querySelector('.container');
+const btnContainer = document.querySelector('.btnContainer');
+const playerScoreNum = document.querySelector('#player');
+const computerScoreNum = document.querySelector('#computer');
+const gameOverScreen = document.querySelector('.gameOver');
+const rndWin = document.querySelector('#rndWin');
+const beatBy = document.querySelector('#beatBy');
+
 let playerScore = 0;
 let computerScore = 0;
 let roundWinner = '';
-
 
 function getComputerChoice() {
     const choice = ['rock', 'paper', 'scissors'];
@@ -17,88 +20,82 @@ function getComputerChoice() {
 };
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    roundWinner = 'tie';
-  }
-  if (
-    (playerSelection === 'rock' && computerSelection === 'scissors') ||
-    (playerSelection === 'paper' && computerSelection === 'rock') ||
-    (playerSelection === 'scissors' && computerSelection === 'paper')
-  ) {
-    playerScore++;
-    roundWinner = 'player';
-  }
-  if (
-    (computerSelection === 'rock' && playerSelection === 'scissors') ||
-    (computerSelection === 'paper' && playerSelection === 'rock') ||
-    (computerSelection === 'scissors' && playerSelection === 'paper')
-  ) {
-    computerScore++;
-    roundWinner = 'computer';
-  }
-  updateScoreMsg(roundWinner, playerSelection, computerSelection);
-};
+    if (playerSelection === computerSelection) {
+      roundWinner = 'tie';
+    }
+    if (
+      (playerSelection === 'rock' && computerSelection === 'scissors') ||
+      (playerSelection === 'paper' && computerSelection === 'rock') ||
+      (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+      playerScore++;
+      roundWinner = 'player';
+    }
+    if (
+      (computerSelection === 'rock' && playerSelection === 'scissors') ||
+      (computerSelection === 'paper' && playerSelection === 'rock') ||
+      (computerSelection === 'scissors' && playerSelection === 'paper')
+    ) {
+      computerScore++;
+      roundWinner = 'computer';
+    }
+    updateScoreMsg(roundWinner, playerSelection, computerSelection);
+  };
 
 function isGameOver() {
   return playerScore === 5 || computerScore === 5
-};
+}; 
 
 function clickInput(playerSelection) {
-  if (isGameOver()) {
-    gameOverScreen.textContent = 'GAME OVER';
-    return
+    if (isGameOver()) {
+      gameOverScreen.textContent = 'GAME OVER';
+      return
+    };
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    updateScore();
   };
-  playRound(playerSelection, computerSelection);
-  updateScore();
+
+function restartGame() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreNum.textContent = 'Player: 0';
+    computerScoreNum.textContent = 'Computer: 0';
+    gameOverScreen.textContent = '';
+    rndWin.textContent = '';
+    beatBy.textContent = '';
+};
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+};
+  
+function updateScore() {
+  if (roundWinner === 'tie') {
+    rndWin.textContent = "It's a tie!"
+  } else if (roundWinner === 'player') {
+    rndWin.textContent = 'You won!'
+  } else if (roundWinner === 'computer') {
+    rndWin.textContent = 'You lost!'
+  }
+
+  playerScoreNum.textContent = `Player: ${playerScore}`;
+  computerScoreNum.textContent = `Computer: ${computerScore}`;
+};
+
+function updateScoreMsg(winner, playerSelection, computerSelection) {
+  if (winner === 'player') {
+    beatBy.textContent = `${capitalizeFirstLetter(playerSelection)} beats ${capitalizeFirstLetter(computerSelection)}!`;
+  }
+  if (winner === 'computer') {
+    beatBy.textContent = `${capitalizeFirstLetter(computerSelection)} beats ${capitalizeFirstLetter(playerSelection)}!`;
+  }
+  if (winner === 'tie') {
+    beatBy.textContent = '';
+  }
 };
 
 rockBtn.addEventListener('click', () => clickInput('rock'));
 paperBtn.addEventListener('click', () => clickInput('paper'));
 scissorsBtn.addEventListener('click', () => clickInput('scissors'));
-
-const div = document.querySelector('div');
-const divThree = document.getElementById('divTwo');
-const playerScorePara = document.createElement('p');
-const computerScorePara = document.createElement('p');
-const divTwo = document.createElement('div');
-const gameOverScreen = document.getElementById('endgame');
-const restartBtn = document.getElementById('restart');
-div.appendChild(divTwo);
-div.appendChild(playerScorePara);
-div.appendChild(computerScorePara);
-
-function updateScoreMsg(winner, playerSelection, computerSelection) {
-  if (winner === 'player') {
-    divTwo.textContent = `${capitalizeFirstLetter(playerSelection)} beats ${capitalizeFirstLetter(computerSelection)}!`;
-  }
-  if (winner === 'computer') {
-    divTwo.textContent = `${capitalizeFirstLetter(computerSelection)} beats ${capitalizeFirstLetter(playerSelection)}!`;
-  }
-};
-
-function updateScore() {
-  if (roundWinner === 'tie') {
-    divThree.textContent = "It's a tie!"
-  } else if (roundWinner === 'player') {
-    divThree.textContent = 'You won!'
-  } else if (roundWinner === 'computer') {
-    divThree.textContent = 'You lost!'
-  }
-
-  playerScorePara.textContent = `Player: ${playerScore}`;
-  computerScorePara.textContent = `Computer: ${computerScore}`;
-};
-
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
-};
-
-function restartGame() {
-  playerScore = 0;
-  computerScore = 0;
-  playerScorePara.textContent = 'Player: 0';
-  computerScorePara.textContent = 'Computer: 0';
-  gameOverScreen.textContent = '';
-};
-
-restartBtn.addEventListener('click', () => restartGame());
+resetBtn.addEventListener('click', () => restartGame());
